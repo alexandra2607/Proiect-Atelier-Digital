496981b2862ed27cfdd02b89b4c7b412d1a3df94
Alexandra State <state.alexandra.gabriela@gmail.com>
2021-03-07 13:56:09 +0200

    Last Update

--- src/Strategy/Main.java
+++ src/Strategy/Main.java
@@ -4,10 +4,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Algeti pachetul: Bronze, Silver sau Gold  ");
+        System.out.println("Algeti pachetul pentru a vizualiza serviciile: Bronze, Silver sau Gold  ");
 
         Scanner nume = new Scanner(System.in);
         String userChoice = nume.nextLine();
+
 
         System.out.println("Ati ales:" + userChoice);
 
@@ -23,6 +24,27 @@ public class Main {
             party.setFoodAndLocation(new GoldPackage());
         }
 
+
+        party.getFoodAndLocation();
+        party.plan();
+
+
+        System.out.println("Scrieti suma pe care o alocati evenimentului: ");
+        Scanner myInput = new Scanner( System.in );
+        int suma = myInput.nextInt();
+
+        if (suma >= 100 && suma < 1000){
+            party.setFoodAndLocation(new BronzePackage());
+            System.out.println("Va sugeram pachetul Bronze");
+        }
+        else if (suma >= 1000 && suma < 10000){
+            party.setFoodAndLocation(new SilverPackage());
+            System.out.println("Va sugeram pachetul Silver");
+        }
+        else if (suma >= 10000) {
+            party.setFoodAndLocation(new GoldPackage());
+            System.out.println("Va sugeram pachetul Gold");
+        }
 
         party.getFoodAndLocation();
         party.plan();
